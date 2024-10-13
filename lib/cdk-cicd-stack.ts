1a7eac72bd01c833e9523a4251025daf657a0035
Collins Orighose <collinsefe@yahoo.com>
2024-10-13 19:48:03 +0100

    added stage and lambda stack

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'AweSomePipeline', {
+    const pipeline = new CodePipeline(this, 'AweSomePipeline', {
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('collinsefe/aws-cdk-cicd', 'cicd-practice'),
@@ -19,7 +20,10 @@ export class CdkCicdStack extends cdk.Stack {
         // primaryOutputDirectory: 'cdk-cicd/cdk.out'
 
       })
-    })
+    });
+  const teststage = pipeline.addStage(new PipelineStage(this 'PipelineTestStage', {
+    stageName: 'test'
+  }))
 
   }
 }
