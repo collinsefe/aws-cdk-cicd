24f337ba3e36733dc17e95a3988f5da69518158a
Collins Orighose <collinsefe@yahoo.com>
2024-10-13 23:55:02 +0100

    cleanup unused items

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,18 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep, CodeBuildStep } from 'aws-cdk-lib/pipelines';
-import { ManualApprovalAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Construct } from 'constructs';
-import { Artifact } from 'aws-cdk-lib/aws-codepipeline';
 import { PipelineStage } from './PipelineStage';
-
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-  //  // Define artifacts for source and build stages
-  //  const sourceArtifact = new Artifact();
-  //  const buildArtifact = new Artifact();
 
 
     const pipeline = new CodePipeline(this, 'AweSomePipeline', {
@@ -25,8 +18,6 @@ export class CdkCicdStack extends cdk.Stack {
           'npm ci',
           'npx cdk synth'
         ],
-        // primaryOutputDirectory: 'cdk-cicd/cdk.out'
-
       })
     });
   const teststage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage', {
@@ -38,35 +29,20 @@ export class CdkCicdStack extends cdk.Stack {
       'npm ci',
       'npm test'
     ]
-  }))
+  }));
+
+  const approvestage = pipeline.addStage(new PipelineStage(this, 'PipelineSApproveStage', {
+    stageName: 'Approve'
+  }));
+
 
   const stagingstage = pipeline.addStage(new PipelineStage(this, 'PipelineStagingStage', {
     stageName: 'Staging'
-  }))  
+  }));
 
-  
-  //  // Add a manual approval step before deploying to Staging
-  //  pipeline.stage('Staging').addAction(new ManualApprovalAction({
-  //   actionName: 'ManualApprovalBeforeStaging',
-  //   runOrder: 1 // Ensure it's the first action in the stage
-  // }));
+  const productionstage = pipeline.addStage(new PipelineStage(this, 'PipelineProdStage', {
+    stageName: 'Production'
+  }));
 
-  // new ManualApprovalAction({
-  //   actionName: 'ManualApprovalBeforeProduction',
-  //   notificationTopic: mySnsTopic,  // Notify users via SNS
-  //   externalEntityLink: 'https://my-internal-review-system.example.com'
-  // });
-
-
-  // // Add the final Production stage
-  // const productionstage = pipeline.addStage(new PipelineStage(this, 'PipelineProductionStage', {
-  //   stageName: 'Production'
-  // }));
-
-  // // Add a manual approval step before deploying to Production
-  // pipeline.stage('Production').addAction(new ManualApprovalAction({
-  //   actionName: 'ManualApprovalBeforeProduction',
-  //   runOrder: 1
-  // }));
 
 }}
