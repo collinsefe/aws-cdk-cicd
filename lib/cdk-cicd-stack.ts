5755b9dae3a8c17dd8885751982def677c6622d8
Collins Orighose <collinsefe@yahoo.com>
2024-10-13 19:11:57 +0100

    initial commit

---
+++ lib/cdk-cicd-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+
+export class CdkCicdStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new CodePipeline(this, 'AweSomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('collinsefe/aws-secured-pipelines', 'main'),
+        commands: [
+          'pwd',
+          'ls -la',
+          'terraform init'
+          // 'cd cdk-cicd',
+          // 'npm ci',
+          // 'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out'
+
+      })
+    })
+
+  }
+}
