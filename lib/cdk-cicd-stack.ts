3fdef0a701eee7842d189a41842772f977d6ca10
Collins Orighose <collinsefe@yahoo.com>
2024-10-13 22:30:25 +0100

    added manual approval and new stage

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -2,10 +2,14 @@ import * as cdk from 'aws-cdk-lib';
 import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { PipelineStage } from './PipelineStage';
+import { aws_codepipeline_actions } from 'aws-cdk-lib'
+// import { iam, Arn } from 'aws-cdk-lib'
+
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
 
     const pipeline = new CodePipeline(this, 'AweSomePipeline', {
       pipelineName: 'AwesomePipeline',
@@ -36,9 +40,24 @@ export class CdkCicdStack extends cdk.Stack {
     stageName: 'Staging'
   }))
 
-  // const prodstage = pipeline.addStage(new PipelineStage(this, 'PipelineStagingProd', {
-  //   stageName: 'Production'
-  // }))
+
+const approveStage = pipeline.addStage(new PipelineStage(this, 'PipelineApproveStage', { 
+  stageName: 'Approve' 
+
+}))
+
+const manualApprovalAction = new aws_codepipeline_actions.ManualApprovalAction({
+  actionName: 'Approve',
+});
+// approveStage.addAction(manualApprovalAction);
+
+// const role = iam.Role.fromRoleArn(this, 'Admin', Arn.format({ service: 'iam', resource: 'role', resourceName: 'Admin' }, this));
+// manualApprovalAction.grantManualApproval(role);
+
+
+  const prodstage = pipeline.addStage(new PipelineStage(this, 'PipelineStagingProd', {
+    stageName: 'Production'
+  }))
 
   }
 }
