713fb8a67a5f2ed86c3d274f62e8632fe66ddc6b
Collins Orighose <collinsefe@yahoo.com>
2024-10-14 23:34:00 +0100

    remove unused stages

--- lib/PipelineStage.ts
+++ lib/PipelineStage.ts
@@ -10,14 +10,14 @@ export class PipelineStage extends Stage {
     constructor(scope: Construct, id: string, props: StageProps) {
         super(scope, id, props)
 
-        new LambdaStack(this, 'LambdaStack', {
-            stageName: props.stageName
-        })
+        // new LambdaStack(this, 'LambdaStack', {
+        //     stageName: props.stageName
+        // })
 
-        new VPCStack(this, 'VPCStack')
+        // new VPCStack(this, 'VPCStack')
 
         new S3ObjectStack(this, 'S3ObjectStack')
 
-        new ECSStack(this, 'ECSStack')
+        // new ECSStack(this, 'ECSStack')
     }
 }
